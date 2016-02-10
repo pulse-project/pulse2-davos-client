c0e3c8fce339a426bbe57562225ee33f0cba2d18
SPO <stephane.pointu@siveo.net>
2016-02-10 14:14:49 +0100

    Cleanup of old revo and various other useless files

--- squashfs_override/rsync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-scp -r $options . user@192.168.71.202:/tmp/
-ssh -r $options ssh user@192.168.71.202 "cp -rf /tmp/davos/* /"
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./davos_build.sh
-cp -f var/lib/pulse2/imaging/davos/* /var/lib/pulse2/imaging/davos/
