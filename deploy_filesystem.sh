4614e6f5df01b0b51baa8127f83168eda6c06b38
Nicolas Lécureuil <neoclust@mageia.org>
2016-05-17 16:01:23 +0200

    Install linux-firmware-nonfree

--- deploy_filesystem.sh
+++ deploy_filesystem.sh
@@ -17,7 +17,7 @@ cp usr/bin/vim.tiny usr/bin/vim
 # Installing additional packages
 mount -t proc none ./proc
 cp /etc/resolv.conf ./etc/resolv.conf
-chroot . bash -c 'apt-get update && apt-get -y install python-minimal libpython-stdlib fusioninventory-agent dos2unix && exit'
+chroot . bash -c 'apt-get update && apt-get -y install python-minimal libpython-stdlib fusioninventory-agent dos2unix linux-firmware-nonfree && exit'
 
 rm -f ./etc/resolv.conf
 umount ./proc
