a0809a476671e081542ff858daed97af1bc13ccc
Nicolas Lécureuil <neoclust@mageia.org>
2018-10-11 15:54:35 +0200

    Fix  typo

--- deploy_filesystem.sh
+++ deploy_filesystem.sh
@@ -15,7 +15,7 @@ mount -t proc none ./proc
 mount devpts /dev/pts -t devpts
 cp /etc/resolv.conf ./etc/resolv.conf
 chroot . ash -c 'tazpkg get-install python clonezilla python-psutil'
-for file in {python-tftpy-0.8.0.tazpkg,fusioninventory-agent-2.4.2.tazpkg perl-universal-require-0.18.tazpkg}; do
+for file in {python-tftpy-0.8.0.tazpkg,fusioninventory-agent-2.4.2.tazpkg,perl-universal-require-0.18.tazpkg}; do
     curl -O https://agents.siveo.net/imaging/${file}
 done
 chroot . ash -c 'tazpkg install *.tazpkg'
