1e58b34f3553d913a0faa87db05cb537163d91a3
spointu <spointu@gmail.com>
2018-10-11 22:37:20 +0900

    Revert "Try minimal rootfs"
    This reverts commit 9f1d8b9167837bcd5261119cc915a0c5bfb36c80.

--- davos_build.sh
+++ davos_build.sh
@@ -3,14 +3,14 @@ set -e
 
 # Edit these line to update slitaz version
 version="rolling"
-flavor="core-5in1"
+flavor="core64"
 base_url="http://mirror.slitaz.org/iso/"
 
 # =============================================================
 # Don't edit anything below these lines
 # =============================================================
 
-file_name=slitaz-${flavor}.iso
+file_name=slitaz-${version}-${flavor}.iso
 full_url=${base_url}/${version}/${file_name}
 old_pwd=$(pwd)
 tempdir=$(mktemp -d)
@@ -35,7 +35,7 @@ mount -o loop $file_name slitaz
 
 # Move needed files to build dir and target dir
 cp slitaz/boot/bzImage64 target/
-cp slitaz/boot/rootfs1.gz64 build/rootfs.gz
+cp slitaz/boot/rootfs.gz build/
 
 # Unmount iso
 umount slitaz
