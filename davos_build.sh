e3240525886de6c1639495974cb39e358d02fdca
spointu <spointu@gmail.com>
2018-10-11 14:22:31 +0900

    Update davos build scripts for Slitaz

--- davos_build.sh
+++ davos_build.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 set -e
 
-# Edit these line to update clonezilla version
-version="20180812-bionic"
-arch="amd64"
-base_url="http://free.nchc.org.tw/clonezilla-live/alternative/stable/"
+# Edit these line to update slitaz version
+version="rolling"
+flavor="core64"
+base_url="http://mirror.slitaz.org/iso/"
 
 # =============================================================
-# Don't edit anything below these lines 
+# Don't edit anything below these lines
 # =============================================================
 
-file_name=clonezilla-live-$version-$arch.zip
-#Uncomment this URL when not using stable folder.
-#full_url=$base_url/$version/$file_name
-full_url=$base_url/$file_name
+file_name=slitaz-${version}-${flavor}.iso
+full_url=${base_url}/${version}/${file_name}
 old_pwd=$(pwd)
 tempdir=$(mktemp -d)
 
-echo "Downloading Clonezilla ..."
-[ ! -f $file_name ] && echo "Downloading Clonezilla" && wget $full_url
+echo "Downloading Slitaz ..."
+[ ! -f $file_name ] && echo "Downloading Slitaz" && wget $full_url
 
 # Sometimes we get html instead of real file
 if [[ ! -f "$file_name" ||  $(stat -c%s "$file_name") -lt 1048576 ]]; then
@@ -27,38 +25,37 @@ if [[ ! -f "$file_name" ||  $(stat -c%s "$file_name") -lt 1048576 ]]; then
 fi
 
 # Entering temp directory
-cp $file_name $tempdir
+mv $file_name $tempdir
 cd $tempdir
 
-mkdir clonezilla build
+mkdir slitaz build target
 
-# Unzip clonezilla zipfile
-unzip "$file_name" -d clonezilla/
+# Mount slitaz iso
+mount -o loop $file_name slitaz
 
-# Move needed files to build dir
-for file in {filesystem.squashfs,initrd.img,vmlinuz}; do
-  mv clonezilla/live/$file build/
-done
+# Move needed files to build dir and target dir
+cp slitaz/boot/bzImage64 target/
+cp slitaz/boot/rootfs.gz build/
+
+# Unmount iso
+umount slitaz
 
 cd build
 
-# Decompressing the squashfs
-unsquashfs filesystem.squashfs  && rm filesystem.squashfs
-cd squashfs-root
+# Decompressing the rootfs
+lzcat rootfs.gz | cpio -id
+rm rootfs.gz
 #sed 's/MULTICAST_ALL_ADDR="224.0.0.1"/MULTICAST_ALL_ADDR="239.254.1.255"/' -i etc/drbl/drbl-ocs.conf
 
 # Run deploy script to patch the filesystem
 $old_pwd/deploy_filesystem.sh $old_pwd/
+
+# Recompress the new rootfs
+find . -print | cpio -o -H newc | gzip -9 > ../target/rootfs.gz
 cd ..
 
-# Recompress the new squashfs
-mksquashfs squashfs-root/ fs.squashfs -noappend -always-use-fragments
-rm -r squashfs-root/
-
-cd ..
-
-# Move builded files to their final dir
-mv -f build/* $old_pwd/var/lib/pulse2/imaging/davos/
+# Move built files to their final dir
+mv -f target/* $old_pwd/var/lib/pulse2/imaging/davos/
 
 # Remove temp dir
 rm -r $tempdir
--- deploy_filesystem.sh
+++ deploy_filesystem.sh
@@ -3,41 +3,26 @@
 
 # =====================================================
 # NOTE :
-#  This script will be run in a temporary squashfs dir
+#  This script will be run in a temporary build dir
 #  $1 is the original dir on the caller script (build.sh)
 # =====================================================
 
 # Copy all fs files to squashfs root
 cp -rvf $1/squashfs_override/* ./
 
-# vim instead of vim.tiny
-# Very useful for debug
-cp usr/bin/vim.tiny usr/bin/vim
-
 # Installing additional packages
 mount -t proc none ./proc
 mount devpts /dev/pts -t devpts
 cp /etc/resolv.conf ./etc/resolv.conf
-chroot . bash -c 'mkdir /boot'
-chroot . bash -c 'apt-get update && apt-get -y install apt-utils python-minimal libpython-stdlib fusioninventory-agent dos2unix linux-firmware python-tftpy python-psutil efivar ash && exit'
-cp /root/partclone_0.2.89-4_amd64.deb /root/clonezilla_3.21.13-1_all.deb /root/drbl_2.20.11-1_all.deb .
-chroot . bash -c 'dpkg -i partclone_0.2.89-4_amd64.deb clonezilla_3.21.13-1_all.deb drbl_2.20.11-1_all.deb'
-chroot . bash -c 'ln -s /usr/lib/systemd/system/sshd.service /etc/systemd/system/multi-user.target.wants/sshd.service'
-chroot . bash -c 'rm -frv /opt/*'
-chroot . bash -c 'echo efivars >> /etc/modules'
-chroot . bash -c 'apt-get autoclean -y '
-chroot . bash -c 'apt-get clean -y '
-chroot . bash -c 'apt-get autoremove -y '
-chroot . bash -c 'find /var/lib/apt/lists/ -maxdepth 1 -type f -exec rm -v {} \;'
+chroot . bash -c 'tazpkg get-install python clonezilla python-psutil'
+for file in {python-tftpy-0.8.0.tazpkg,fusioninventory-agent-2.4.2.tazpkg}; do
+    curl -O https://agents.siveo.net/imaging/${file}
+done
+chroot . bash -c 'tazpkg install *.tazpkg'
 
+rm -f ./*.tazpkg
 rm -f ./etc/resolv.conf
 umount ./proc
-
-# Removing APT cache
-rm -rf var/cache/apt
-
-# Skip keymap selection (in deploy mode)
-[ -z $DEBUG ] && cat /dev/null > etc/ocs/ocs-live.d/S05-lang-kbd-conf
 
 # Disable kernel low level messages in the console
 sed -i 's/^#kernel\.printk.*/kernel.printk = 3 4 1 3/' etc/sysctl.conf
